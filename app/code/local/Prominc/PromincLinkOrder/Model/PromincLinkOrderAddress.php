486429c1f132d32db8f08b0133a4e9d6e21cf4a0
PromInc <PromInc@hotmail.com>
2017-11-01 05:16:46 -0500

    Update PromincLinkOrderAddress.php

--- app/code/local/Prominc/PromincLinkOrder/Model/PromincLinkOrderAddress.php
+++ app/code/local/Prominc/PromincLinkOrder/Model/PromincLinkOrderAddress.php
@@ -1,8 +1,10 @@
 <?php
 class Prominc_PromincLinkOrder_Model_PromincLinkOrderAddress extends Mage_Core_Model_Abstract
 {
+
   public function _construct() {
     parent::_construct();
     $this->_init('prominclinkorder/prominclinkorderaddress');
   }
+
 }
