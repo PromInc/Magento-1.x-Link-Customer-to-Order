350fac3f326ba2473e92b6ba04d3498f6a833c70
PromInc <PromInc@hotmail.com>
2017-11-01 05:15:19 -0500

    Create PromincLinkOrderAddress.php

---
+++ app/code/local/Prominc/PromincLinkOrder/Model/PromincLinkOrderAddress.php
@@ -0,0 +1,8 @@
+<?php
+class Prominc_PromincLinkOrder_Model_PromincLinkOrderAddress extends Mage_Core_Model_Abstract
+{
+  public function _construct() {
+    parent::_construct();
+    $this->_init('prominclinkorder/prominclinkorderaddress');
+  }
+}
